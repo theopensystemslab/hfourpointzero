cccaecdff12f0854388796655205da0b5d0b2ec9
John Rees <john@bitsushi.com>
2019-11-18 13:51:08 +0100

    draw polygon

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,23 +1,139 @@
+import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
+import { area, polygon } from "@turf/turf";
 import React, { useState } from "react";
 import ReactMapGL from "react-map-gl";
+import { Editor, EditorModes } from "react-map-gl-draw";
+import create from "zustand";
+import shallow from "zustand/shallow";
+import { getEditHandleStyle, getFeatureStyle } from "./styles";
+
+const [useStore, api] = create((set, get) => ({
+  map: undefined,
+  mode: EditorModes.READ_ONLY,
+  selectedFeatureIndex: undefined,
+  deleteSelected: () => {
+    const { selectedFeatureIndex, map } = get();
+    if (map && parseInt(selectedFeatureIndex) >= 0) {
+      map.deleteFeatures(selectedFeatureIndex);
+      set({ selectedFeatureIndex: undefined });
+    }
+  },
+  // setViewport: viewport => set({ viewport }),
+  setMap: map => set({ map }),
+  setMode: mode => set({ mode }),
+  setSelectedFeatureIndex: selectedFeatureIndex => set({ selectedFeatureIndex })
+}));
+
+const DrawControls: React.FC = () => {
+  const { deleteSelected, setMode, map, selectedFeatureIndex } = useStore(
+    state => ({
+      setMode: state.setMode,
+      deleteSelected: state.deleteSelected,
+      map: state.map,
+      selectedFeatureIndex: state.selectedFeatureIndex
+    })
+  );
+
+  return (
+    <div className="mapboxgl-ctrl-top-left">
+      <div className="mapboxgl-ctrl-group mapboxgl-ctrl">
+        {map && parseInt(selectedFeatureIndex) >= 0 && (
+          <button
+            className="mapbox-gl-draw_ctrl-draw-btn mapbox-gl-draw_trash"
+            title="Delete"
+            onClick={deleteSelected}
+          />
+        )}
+        {map && isNaN(selectedFeatureIndex) && (
+          <button
+            className="mapbox-gl-draw_ctrl-draw-btn mapbox-gl-draw_polygon"
+            title="Polygon tool (p)"
+            onClick={() => setMode(EditorModes.DRAW_POLYGON)}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+const Continue = () => {
+  const map = useStore(state => state.map);
+  const shape = map.getFeatures()[0].geometry.coordinates;
+  return (
+    <div id="continue">
+      <p>{area(polygon(shape)).toFixed(1)}m²</p>
+      <h1>Continue</h1>
+    </div>
+  );
+};
+
+const RenderEditor = () => {
+  const {
+    mode,
+    selectedFeatureIndex,
+    setMap,
+    setMode,
+    setSelectedFeatureIndex
+  } = useStore(
+    state => ({
+      mode: state.mode,
+      selectedFeatureIndex: state.selectedFeatureIndex,
+      setMap: state.setMap,
+      setMode: state.setMode,
+      setSelectedFeatureIndex: state.setSelectedFeatureIndex
+    }),
+    shallow
+  );
+
+  return (
+    <>
+      <Editor
+        clickRadius={12}
+        editHandleShape={"circle"}
+        editHandleStyle={getEditHandleStyle}
+        featureStyle={getFeatureStyle}
+        mode={mode}
+        onSelect={options => {
+          setSelectedFeatureIndex(options && options.selectedFeatureIndex);
+        }}
+        onUpdate={e => {
+          if (e.editType === "addFeature") {
+            setMode(EditorModes.EDITING);
+            setSelectedFeatureIndex(undefined);
+          }
+        }}
+        ref={el => el && setMap(el)}
+        // style={{ width: "100%", height: "100%" }}
+      />
+      <DrawControls />
+      {parseInt(selectedFeatureIndex) >= 0 && <Continue />}
+    </>
+  );
+};
 
 const Map: React.FC = () => {
-  const [state, setState] = useState({
-    viewport: {
-      longitude: 4,
-      latitude: 50,
-      zoom: 4
-    }
+  const [viewport, setViewport] = useState({
+    longitude: 4,
+    latitude: 50,
+    zoom: 4
   });
 
   return (
     <ReactMapGL
-      {...state.viewport}
+      {...viewport}
       width="100%"
       height="100vh"
       mapStyle="mapbox://styles/mapbox/satellite-v9"
-      onViewportChange={(viewport: any) => setState({ viewport })}
-    />
+      onViewportChange={viewport => {
+        setViewport({
+          longitude: viewport.longitude,
+          latitude: viewport.latitude,
+          zoom: viewport.zoom
+        });
+      }}
+    >
+      <RenderEditor />
+    </ReactMapGL>
   );
 };
 
---
+++ src/useMap.tsx
@@ -0,0 +1,21 @@
+import React, { createContext, useContext, useState } from "react";
+
+// create context object
+const MapContext = createContext(null);
+
+// handle state in your provider and pass it as the value
+export function MapProvider(props) {
+  const [map, setMap] = useState();
+  return <MapContext.Provider value={[map, setMap]} {...props} />;
+}
+
+// expose a helper hook to easily grab the state anywhere in your app
+// This is an excellent pattern to share state without redux - and be
+// wary of how you can optimise it:
+// https://kentcdodds.com/blog/how-to-optimize-your-context-value
+export function useMap() {
+  const context = useContext(MapContext);
+  if (context === undefined)
+    throw Error("You forgot to wrap your app with <MapProvider />");
+  return context;
+}
